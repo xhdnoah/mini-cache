df574498793d7dc8189cd709e37c5af7cdded509
xhdnoah <xhd.github@gmail.com>
2021-11-17 16:52:54 +0800

    feat: protobuf

--- group.go
+++ group.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"fmt"
 	"log"
+	pb "mini-cache/cachepb"
 	"mini-cache/singleflight"
 	"sync"
 )
@@ -122,9 +123,14 @@ func (g *Group) RegisterPeers(peers PeerPicker) {
 
 // 使用实现了 PeerGetter 接口的 httpGetter 从访问远程节点获取缓存值
 func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
-	bytes, err := peer.Get(g.name, key)
+	req := &pb.Request{
+		Group: g.name,
+		Key:   key,
+	}
+	res := &pb.Response{}
+	err := peer.Get(req, res)
 	if err != nil {
 		return ByteView{}, err
 	}
-	return ByteView{b: bytes}, nil
+	return ByteView{b: res.Value}, nil
 }
